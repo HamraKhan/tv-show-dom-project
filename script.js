ff756a272c1807690184d4915d698113e84f1e2a
Hamra <hamra.khan.b@gmail.com>
2022-02-22 23:08:16 +0000

    - Added footer element and linked it to TVMaze site. - Created function formatNumber() to prepend a 0 to single digit numbers to display season/episode -  Created elements and placed them in their respective functions. Called those functions inside a loop in makePageForEpisodes() .

--- script.js
+++ script.js
@@ -4,9 +4,66 @@ function setup() {
   makePageForEpisodes(allEpisodes);
 }
 
+function formatNumber(episodeNumber) {
+  return episodeNumber.toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+}
+
+function createTVEpisodeDiv() {
+  const tvEpisode = document.createElement("div");
+  tvEpisode.classList.add("tv-episode");
+  return tvEpisode;
+}
+
+function createEpisodeTitle(tvEpisodeDiv, episode) {
+  const episodeTitle = document.createElement("h1");
+  episodeTitle.classList.add("episode-title");
+
+  episodeTitle.innerHTML = `${episode.name} - S${formatNumber(
+    episode.season
+  )}E${formatNumber(episode.number)}`;
+
+  tvEpisodeDiv.appendChild(episodeTitle);
+}
+
+function createEpisodeImage(tvEpisodeDiv, episode) {
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("img-div");
+
+  const image = document.createElement("img");
+  image.src = episode.image.medium;
+  imgDiv.appendChild(image);
+
+  tvEpisodeDiv.appendChild(imgDiv);
+}
+
+function createEpisodeDescription(tvEpisodeDiv, episode) {
+  const descDiv = document.createElement("div");
+  descDiv.classList.add("episode-description");
+
+  descDiv.insertAdjacentHTML("beforeend", episode.summary);
+
+  tvEpisodeDiv.appendChild(descDiv);
+}
+
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  episodeList.forEach(episode => {
+    
+    const tvEpisodeDiv = createTVEpisodeDiv();
+
+    createEpisodeTitle(tvEpisodeDiv, episode);
+
+    createEpisodeImage(tvEpisodeDiv, episode);
+   
+    createEpisodeDescription(tvEpisodeDiv, episode);
+
+    rootElem.appendChild(tvEpisodeDiv);
+  });
 }
 
 window.onload = setup;
